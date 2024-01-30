d8cac6b11a7ed46d79b4a69e2c4b05cc8eb638f0
hyktv <hafsa.yktv@gmail.com>
2024-01-29 20:07:56 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GA_Constructors_Hafsa
+{
+    //Hafsa Mohamed
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

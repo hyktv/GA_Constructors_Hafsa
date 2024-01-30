6f4d490fca134cb00118718b546ff9cd4bb2ab33
hyktv <hafsa.yktv@gmail.com>
2024-01-29 21:37:24 -0800

    finished

---
+++ Book.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Constructors_Hafsa
+{
+    internal class Book
+    {
+        //feilds
+        private string author;
+        private string title;
+        private string illustrator;
+
+        public string Title
+        {
+            get { return title; }
+        }
+        public string Author
+        {
+            get { return author; }
+        }
+        public string Illustrator
+        {
+            get { return illustrator; }
+        }
+       
+
+        //a constructor - a method in a class that is called when an oject of the class is created
+
+        //default constructor - provided by c# if no constructors are defined. it initializes the object without setting any feilds
+        public Book()
+        {
+            //initialize feilds or set default values
+            title = "Default Title";
+            author = "Default Author";
+            illustrator = "Default illustrator";
+        }
+
+        //custom constructor with parameters to initialize feilds
+        public Book(string title, string author, string illustrator)
+        {
+            //creating this constructor removes the default one so now we ensure that specific data has to be passed to make the instnace of this class
+            this.title = title;
+            this.author = author;
+            this.illustrator = illustrator;
+        }
+
+        // Overloaded constructor with different parameters
+        public Book(string title)
+        {
+            // Set the title using the provided parameter
+            this.title = title;
+            // Assign a default author value
+            this.author = "No Author Given";
+        }
+
+        // Field initialization in constructors ensures objects have valid states
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book newBook = new Book(); //this is calling a constructor the default one in this case
+
+            Book bookAuthorAndName = new Book("Night Watch", "Terry Pratchett", "Jaden Smith"); //this is calling the custom contstructor
+
+            Book bookNameOnly = new Book("Wyrd Sisters"); //this is overloading your constructor
+
+            Book book1 = new Book("1984", "George Orwell", "Hafsa");
+            Book book2 = new Book("The Hobbit");
+
+            Console.WriteLine(bookNameOnly.Author);
+            // Prints No Author Given
+
+            // Displaying initialized values
+            Console.WriteLine($"Book with Author and Title: Title - {bookAuthorAndName.Title}, Author - {bookAuthorAndName.Author}, Illustrator - {bookAuthorAndName.Illustrator}");
+            Console.WriteLine($"Book with Title Only: Title - {bookNameOnly.Title}, Author - {bookNameOnly.Author}");
+            Console.WriteLine($"Book 1: Title - {book1.Title}, Author - {book1.Author}, Illustrator - {book1.Illustrator}");
+            Console.WriteLine($"Book 2: Title - {book2.Title}");
         }
     }
 }
